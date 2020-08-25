e73acb88aca8583c66ebd4bb2d13997c79c1eaad
Janeal Pimentel <ceejaypimentel@hotmail.ca>
2020-08-24 21:40:26 -0500

    Adds Link model and migrations

---
+++ db/migrate/20200825023929_create_links.rb
@@ -0,0 +1,11 @@
+class CreateLinks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :links do |t|
+      t.string :url
+      t.string :slug
+
+      t.timestamps
+    end
+    add_index :links, :slug, unique: true
+  end
+end
